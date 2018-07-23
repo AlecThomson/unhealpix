a39f07163722b2c672e8f8e34a2a24c6879e646b
Alec Thomson <alec.thomson@anu.edu.au>
2018-07-23 16:16:33 +1000

    Added argparse

--- unhealpix.py
+++ unhealpix.py
@@ -2,16 +2,42 @@
 
 from astropy.io import fits
 from reproject import reproject_from_healpix, reproject_to_healpix
-import sys
+import argparse
+
+# Help string to be shown using the -h option
+descStr = """
+Regrid a HEALPIX FITS file to target FITS image.
+A simple implementation of Astropy and Reproject FITS handling.
+This script reprojects a HEALPIX projected FITS file to an image projection.
+Beware of any projection issues this may cause!
+
+"""
+
+# Parse the command line options
+parser = argparse.ArgumentParser(description=descStr,
+                             formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("fitsHEAL", metavar="HEALPIX.fits", nargs=1,
+                    help="FITS map in HEALPIX format.")
+parser.add_argument("template", metavar="template.fits", nargs=1,
+                    help="Template FITS map to project onto.")
+parser.add_argument("out", metavar="output.fits", nargs=1,
+                    help="FITS map to save output to.")
+parser.add_argument("-e", dest="Extension", default=1,
+                    help="Extension to select from HEALPIX file.")
+parser.add_argument("-f", dest="field", default=0,
+                    help="Select field within extension.")
+parser.add_argument("-c", dest="COORDSYS", default='galactic',
+                    help="Specify COORDSYS card.")
+args = parser.parse_args()
 
 
 print 'Reading files...'
 # Read in HEALPIX file
-outf = sys.argv[1]
-hdu2 = fits.open(outf)[1]
+outf = args.fitsHEAL[0]
+hdu2 = fits.open(outf)[args.Extension]
 
 # Read in target fits file
-inf = sys.argv[2]
+inf = args.template[0]
 hdu1 = fits.open(inf)[0]
 
 print 'Generating header...'
@@ -36,14 +62,14 @@ for i in target_header:
 # Force the axes to be 2D
 target_header['NAXIS'] = 2
 
-hdu2.header['COORDSYS'] = 'galactic'
+hdu2.header['COORDSYS'] = args.COORDSYS
 # Regrid
 print 'Regridding...'
-array, footprint = reproject_from_healpix(hdu2, target_header, field = 0)
+array, footprint = reproject_from_healpix(hdu2, target_header, field = int(args.field))
 
 # Save the file!
 
-outf1 = sys.argv[3]
+outf1 = args.out[0]
 print 'Saving output to '+ outf1
 hdu = fits.PrimaryHDU(array,header=target_header)
 hdul = fits.HDUList([hdu])
